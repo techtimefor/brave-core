1e84e8c0b38317d032388b1d7516b45689d3c71e
Jacob Lamont <jlamont@brave.com>
2020-10-19 16:46:30 -0400

    moves API fetching to NTP container

--- components/brave_new_tab_ui/storage/new_tab_storage.ts
+++ components/brave_new_tab_ui/storage/new_tab_storage.ts
@@ -113,6 +113,7 @@ export const defaultState: NewTab.State = {
     optInTotal: false,
     optInBTCPrice: false,
     optInMarkets: false,
+    fetchStatus: null,
     tickerPrices: {},
     losersGainers: {},
     supportedPairs: {},
